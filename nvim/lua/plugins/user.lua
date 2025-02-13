a4d1f719e95789a2d95c2e6e23f88076cfcf8610
kush.patel <kush@V2XRDTED5JYG24.vmscrdte.com>
2025-02-13 18:34:30 -0500

    refactored configs

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,173 +1,212 @@
-
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
 return {
---------------------------------------------------------------------------------
--------------------------------- benlubas/molten-nvim --------------------------
---------------------------------------------------------------------------------
-{
-  "benlubas/molten-nvim",
-  version = "^1.0.0",
-  build = ":UpdateRemotePlugins",
-  init = function()
-    vim.g.molten_auto_open_output = false -- cannot be true if molten_image_provider = "wezterm"
-    vim.g.molten_output_show_more = true
-    vim.g.molten_image_provider = "wezterm"
-    vim.g.molten_output_virt_lines = true
-    vim.g.molten_split_direction = "right" --direction of the output window, options are "right", "left", "top", "bottom"
-    vim.g.molten_split_size = 40 --(0-100) % size of the screen dedicated to the output window
-    vim.g.molten_virt_text_output = true
-    vim.g.molten_use_border_highlights = true
-    vim.g.molten_virt_lines_off_by_1 = true
-    vim.g.molten_auto_image_popup = false
-  end,
-  config = function()
-    vim.keymap.set("n", "<localleader>rk", ":MoltenRestart<CR>", { silent = true })
-    vim.keymap.set("n", "<localleader>ri", ":MoltenInit<CR>", { silent = true })
-  end,
-},
---------------------------------------------------------------------------------
--------------------------------- 3rd/image.nvim --------------------------------
---------------------------------------------------------------------------------
--- {
---     "3rd/image.nvim",
---     opts = {
---       backend = "kitty",
---       integrations = {},
---       max_width = 100,
---       max_height = 12,
---       max_height_window_percentage = math.huge,
---       max_width_window_percentage = math.huge,
---       window_overlap_clear_enabled = true,
---       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
--- },
---------------------------------------------------------------------------------
------------------------------ GCBallesteros/jupytext.nvim ----------------------
---------------------------------------------------------------------------------
-{
-  "GCBallesteros/jupytext.nvim",
-  config = function()
-    require("jupytext").setup({
-      style = "markdown",
-      output_extension = "md",
-      force_ft = "markdown",
-    })
-  end,
-},
---------------------------------------------------------------------------------
------------------------------- quarto-dev/quarto-nvim -------------------------
---------------------------------------------------------------------------------
-{
-  "quarto-dev/quarto-nvim",
-  dependencies = {
-    "jmbuhr/otter.nvim",
-    "nvim-treesitter/nvim-treesitter",
+  --------------------------------------------------------------------------------
+  ------------------------------- Themes Setup ---------------------------------
+  --------------------------------------------------------------------------------
+  { "folke/tokyonight.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
   },
-  ft = { "quarto", "markdown" },
-  config = function()
-    local quarto = require("quarto")
-    quarto.setup({
-      lspFeatures = {
-        languages = { "r", "python", "rust" },
-        chunks = "all",
-        diagnostics = {
-          enabled = true,
-          triggers = { "BufWritePost" },
-        },
-        completion = { enabled = true },
-      },
-      keymap = {
-        hover = "H",
-        definition = "gd",
-        rename = "<leader>rn",
-        references = "gr",
-        format = "<leader>gf",
-      },
-      codeRunner = {
-        enabled = true,
-        default_method = "molten",
-      },
-    })
+  {
+    "loctvl842/monokai-pro.nvim",
+    priority = 1000,
+  },
 
-    local runner = require("quarto.runner")
-    vim.keymap.set("n", "<localleader>rc", runner.run_cell, { silent = true, desc = "Run current cell" })
-    vim.keymap.set("n", "<localleader>ra", runner.run_above, { silent = true, desc = "Run cells above" })
-    vim.keymap.set("n", "<localleader>rA", runner.run_all, { silent = true, desc = "Run all cells" })
-    vim.keymap.set("n", "<localleader>rl", runner.run_line, { silent = true, desc = "Run current line" })
-    vim.keymap.set("v", "<localleader>rr", runner.run_range, { silent = true, desc = "Run selected range" })
-    vim.keymap.set("n", "<localleader>RA", function()
-      runner.run_all(true)
-    end, { silent = true })
-  end,
-},
---------------------------------------------------------------------------------
-------------------------------- ellisonleao/dotenv.nvim -----------------------
---------------------------------------------------------------------------------
-{
-  "ellisonleao/dotenv.nvim",
-  config = function()
-    require('dotenv').setup({
-      enable_on_load = true,
-      verbose = false,
-    })
-  end,
-},
---------------------------------------------------------------------------------
------------------------------ nvim-telescope/telescope.nvim --------------------
---------------------------------------------------------------------------------
-{
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    pickers = {
-      colorscheme = {
-        enable_preview = true,
+  --------------------------------------------------------------------------------
+  ------------------------------- LSP and REPL Providers -----------------------
+  --------------------------------------------------------------------------------
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local function find_python_venv()
+        local venv = vim.fn.finddir('.venv', '.;')
+        if venv == '' then
+          return ''
+        end
+        local python_path = venv .. '/bin/python3'
+        print("Using Python interpreter: " .. python_path)
+        return python_path
+      end
+
+      vim.g.python3_host_prog = find_python_venv()
+    end,
+  },
+  {
+    'willothy/wezterm.nvim',
+    config = true
+  },
+  {
+    "Vigemus/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+      local view = require("iron.view")
+      local common = require("iron.fts.common")
+
+      iron.setup {
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            sh = { command = {"zsh"} },
+            python = {
+              command = { "python3" },
+              format = common.bracketed_paste_python,
+              block_deviders = { "# %%", "#%%" },
+            }
+          },
+          repl_filetype = function(bufnr, ft)
+            return ft
+          end,
+          repl_open_cmd = view.split.vertical.botright(50),
+        },
+        highlight = {
+          italic = true
+        },
+        ignore_blank_lines = true,
+      }
+
+      -- Key Mappings for REPL Functions
+      vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>', { desc = 'Focus REPL' })
+      vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>', { desc = 'Hide REPL' })
+    end
+  },
+
+  --------------------------------------------------------------------------------
+  ----------------------------- Notebook and Jupyter Integration ---------------
+  --------------------------------------------------------------------------------
+  {
+    "GCBallesteros/jupytext.nvim",
+    config = function()
+      require("jupytext").setup({
+        style = "hydrogen",
+        output_extension = "auto",
+        force_ft = nil,
+      })
+    end,
+  },
+
+  {
+    "GCBallesteros/NotebookNavigator.nvim",
+    keys = {
+      { "]h", function() require("notebook-navigator").move_cell "d" end, { desc = "Move cell down" } },
+      { "[h", function() require("notebook-navigator").move_cell "u" end, { desc = "Move cell up" } },
+      { "<localleader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>", { desc = "Run current cell" } },
+      { "<localleader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>", { desc = "Run and move to next cell" } },
+    },
+    dependencies = {
+      "echasnovski/mini.comment",
+      "hkupty/iron.nvim", -- REPL provider
+      "anuvyklack/hydra.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      local nn = require "notebook-navigator"
+      nn.setup({ activate_hydra_keys = "<leader>h" })
+    end,
+  },
+
+  --------------------------------------------------------------------------------
+  ------------------------------ Quarto Integration Setup ----------------------
+  --------------------------------------------------------------------------------
+  {
+    "quarto-dev/quarto-nvim",
+    dependencies = {
+      "jmbuhr/otter.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = { "quarto", "markdown" },
+    config = function()
+      local quarto = require("quarto")
+      quarto.setup({
+        lspFeatures = {
+          languages = { "r", "python", "rust" },
+          chunks = "all",
+          diagnostics = {
+            enabled = true,
+            triggers = { "BufWritePost" },
+          },
+          completion = { enabled = true },
+        },
+        keymap = {
+          hover = "H",
+          definition = "gd",
+          rename = "<leader>rn",
+          references = "gr",
+          format = "<leader>gf",
+        },
+        codeRunner = {
+          enabled = true,
+        },
+      })
+    end,
+  },
+
+  --------------------------------------------------------------------------------
+  ------------------------------- Utility Plugins Setup -----------------------
+  --------------------------------------------------------------------------------
+  {
+    "ellisonleao/dotenv.nvim",
+    config = function()
+      require('dotenv').setup({
+        enable_on_load = true,
+        verbose = false,
+      })
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
       },
     },
   },
-},
---------------------------------------------------------------------------------
--------------------------------- folke/tokyonight.nvim ------------------------
---------------------------------------------------------------------------------
-{ "folke/tokyonight.nvim" },
---------------------------------------------------------------------------------
-------------------------------- catppuccin/nvim ------------------------------
---------------------------------------------------------------------------------
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
---------------------------------------------------------------------------------
----------------------------- olimorris/onedarkpro.nvim -----------------------
---------------------------------------------------------------------------------
-{
-  "olimorris/onedarkpro.nvim",
-  priority = 1000,
-},
-{
-  "loctvl842/monokai-pro.nvim",
-  priority = 1000,
-},
---------------------------------------------------------------------------------
----------------------------- neovim/nvim-lspconfig ----------------------------
---------------------------------------------------------------------------------
-{
-  "neovim/nvim-lspconfig",
-  config = function()
-    local function find_python_venv()
-      local venv = vim.fn.finddir('.venv', '.;')
-      if venv == '' then
-        return ''
-      end
-      local python_path = venv .. '/bin/python3'
-      print("Using Python interpreter: " .. python_path)
-      return python_path
-    end
 
-    vim.g.python3_host_prog = find_python_venv()
-  end,
-},
---------------------------------------------------------------------------------
-{
-    'willothy/wezterm.nvim',
-    config = true
-},
+  {
+    "hat0uma/csvview.nvim",
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  },
+
+  --------------------------------------------------------------------------------
+  ----------------------------- Molten and Image Providers --------------------
+  --------------------------------------------------------------------------------
+  -- Add configuration here as needed
+  -- {
+  --   "benlubas/molten-nvim",
+  --   version = "^1.0.0",
+  --   build = ":UpdateRemotePlugins",
+  --   init = function()
+  --     vim.g.molten_auto_open_output = false
+  --     vim.g.molten_output_show_more = true
+  --     vim.g.molten_image_provider = "wezterm"
+  --     vim.g.molten_output_virt_lines = true
+  --     vim.g.molten_split_direction = "right"
+  --     vim.g.molten_split_size = 40
+  --     vim.g.molten_virt_text_output = true
+  --     vim.g.molten_use_border_highlights = true
+  --     vim.g.molten_virt_lines_off_by_1 = true
+  --     vim.g.molten_auto_image_popup = false
+  --   end,
+  --   config = function()
+  --     vim.keymap.set("n", "<localleader>rk", ":MoltenRestart<CR>", { silent = true, desc = "Restart Molten" })
+  --     vim.keymap.set("n", "<localleader>ri", ":MoltenInit<CR>", { silent = true, desc = "Initialize Molten" })
+  --   end,
+  -- },
 }
-
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,28 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local mod = {
+  SUPER = 'ALT',
+  SUPER_REV = 'ALT|CTRL'
+}
+
+-- Define the custom event
+wezterm.on('tabs.toggle-tab-bar', function(window, _pane)
+  local effective_config = window:effective_config()
+  window:set_config_overrides({
+    enable_tab_bar = not effective_config.enable_tab_bar,
+  })
+end)
 
 return {
   -- Set font to FiraCode Nerd Font Mono
-  font = wezterm.font("FiraCode Nerd Font Mono", {weight = "Medium"}),
+  font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Medium" }),
   font_size = 12.0, -- Adjust as needed
-  
+
   color_scheme = "Gruvbox Dark",
 
   colors = {
-    cursor_fg = "white", 
+    cursor_fg = "white",
     cursor_bg = "white",
   },
 
@@ -18,6 +32,14 @@ return {
     top = 0,
     bottom = 0,
   },
-  enable_tab_bar = false,
+  default_prog = { 'pwsh.exe' },
 
+  keys = {
+    {
+      key = '9',
+      mods = mod.SUPER,
+      action = act.EmitEvent('tabs.toggle-tab-bar'),
+    },
+  }
 }
+
