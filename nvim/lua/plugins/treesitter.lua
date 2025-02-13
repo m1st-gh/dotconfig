91e5635ea5f5cb8884a1d31f2f035353993cc3b2
kush.patel <kush@V2XRDTED5JYG24.vmscrdte.com>
2025-02-12 22:56:03 -0500

    Added Molten

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -7,9 +5,22 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
+          "markdown",
+          "markdown_inline",
+          "python",
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighing = false,
+        },    incremental_selection = {
+      enable = true,  -- Enable incremental selection (for selecting code blocks)
+      keymaps = {
+        init_selection = "<CR>",  -- Key for starting selection
+        node_incremental = "<TAB>",  -- Key for expanding selection
+        node_decremental = "<S-TAB>",  -- Key for shrinking selection
+        scope_incremental = "<C-Space>",  -- Key for expanding scope selection
+      },
     },
   },
 }
+
