1396bd9fdb7877cfc572a3b72e79641b9d667004
Kush Patel <kushpatel169@gmail.com>
2025-02-13 22:51:16 -0500

    pt2

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -42,15 +40,9 @@ return {
     -- Mappings can be configured through AstroCore as well.
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
           function()
             require("astroui.status.heirline").buffer_picker(
@@ -59,13 +51,27 @@ return {
           end,
           desc = "Close buffer from tabline",
         },
-
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<localleader>n"] = { desc = "󰠮 Notebook" },
+        ["<localleader>nq"] = { "<cmd>lua require('nabla').toggle_virt()<cr>", desc = "Toggle math equations" },
+        ["<localleader>nd"] = {
+          function() require("notebook-navigator").move_cell "d" end,
+          desc = "Move cell down",
+        },
+        ["<localleader>nu"] = {
+          function() require("notebook-navigator").move_cell "u" end,
+          desc = "Move cell up",
+        },
+        ["<localleader>nr"] = {
+          "<cmd>lua require('notebook-navigator').run_cell()<cr>",
+          desc = "Run current cell",
+        },
+        ["<localleader>nm"] = {
+          "<cmd>lua require('notebook-navigator').run_and_move()<cr>",
+          desc = "Run and move next cell",
+        },
+        ["<localleader>r"] = { desc = " REPL" },
+        ["<localleader>rf"] = { "<cmd>IronFocus<cr>", desc = "Focus REPL" },
+        ["<localleader>rh"] = { "<cmd>IronHide<cr>", desc = "Hide REPL" },
       },
     },
   },
