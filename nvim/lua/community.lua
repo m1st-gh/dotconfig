d3a993cd7191d1e6e06b0526da40eade1c272365
Kush Patel <kushpatel169@gmail.com>
2025-03-02 00:10:58 -0500

    Round 129

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,12 +5,25 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.search.grug-far-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+  { import = "astrocommunity.pack.quarto" },
   { import = "astrocommunity.pack.lua" }, -- import/override with your plugins folder
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.typescript" }, -- import/override with your plugins folder
-  { import = "astrocommunity.pack.python" }, -- import/override with your plugins folder
   { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+  { import = "astrocommunity.utility.hover-nvim" },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "claude-3.7-sonnet-thought",
+    },
+  },
   {
     "loctvl842/monokai-pro.nvim",
     config = function()
