bdadcdd647e1d6aa8bff50680a3de8705965eb51
Kush Patel <kushpatel169@gmail.com>
2025-05-14 20:12:46 -0400

    updated nvtree

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,6 +5,17 @@ return {
     event = { "BufWritePre" },
     opts = require "configs.conform",
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup {
+        filters = {
+          dotfiles = true,
+          git_ignored = true,
+        },
+      }
+    end,
+  },
 
   -- LSP config
   {
