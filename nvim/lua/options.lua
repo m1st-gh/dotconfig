b2724e370b629542ec07a03ac762e06480906aa1
Kush Patel <kush.patel@gov2x.com>
2025-07-21 09:12:18 -0400

    Remove sql wait time

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,8 +4,10 @@ require "nvchad.options"
 
 local o = vim.o
 local cmd = vim.cmd
+local g = vim.g
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.foldlevelstart = 99
 o.relativenumber = true
 o.guicursor = "n-v-c:block-blinkon250-blinkoff250,i:ver25-blinkon250-blinkoff250"
+g.omni_sql_no_default_maps = 1
